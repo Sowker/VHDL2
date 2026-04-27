7885c25a3f335ec95ac404989a35a8cb0f7489e9
Sowker <remi.pyt@gmail.com>
2026-04-27 16:34:59 +0000

    Frequency Divisor done

--- LFSR/LFSR.vhd
+++ LFSR/LFSR.vhd
@@ -13,12 +13,29 @@ end LFSRCore;
 
 
 architecture LFSRCore_Arch of LFSRCore is
+    -- le LFSR est composé d'un diviseur de tension 
+    -- D Flip FLop
+    -- XOR opperation
+    signal counter :integer range 0 to 5 := 0;  -- Here our signal is divised by 5 (maybe not good idk)
+    signal FINALCLK : std_logic := '0';
 
 begin
     MyDiviseurFreq_Proc : process(CLK)
-    variable : 
+
     begin 
-        if rising_edge(CLK) then
+        if RESET = '1' then
+            counter <= 0;
+            FINALCLK <= '0';
+
+        elsif rising_edge(CLK) then
+            if counter = 5 then
+                counter <= 0;
+                FINALCLK <= not(FINALCLK);
+            end if;
+            counter <= counter + 1;
+        end if;
+    end;
+
 
     MyLFSRore_Proc : process (CLK, RESET, ENABLE)
     begin
