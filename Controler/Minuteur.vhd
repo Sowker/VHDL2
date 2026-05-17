de39a64ebd16e7bd05d42b248c5e4b9977f6b779
Sowker <remi.pyt@gmail.com>
2026-05-17 16:23:04 +0000

    Controler finished

--- Controler/Minuteur.vhd
+++ Controler/Minuteur.vhd
@@ -26,19 +26,19 @@ begin
                 counter <= counter + 1;
                 
                 if SW_LEVEL = "00" then
-                    if counter = 40 then   -- 400000000
+                    if counter = 400000000 then   -- 400 000 000
                         S <= '1';
                     end if;
                 elsif SW_LEVEL = "01" then
-                    if counter = 20 then  -- 200000000
+                    if counter = 200000000 then  -- 200 000 000
                         S <= '1';
                     end if;
                 elsif SW_LEVEL = "10" then
-                    if counter = 10 then  -- 100000000
+                    if counter = 100000000 then  -- 100 000 000
                         S <= '1';
                     end if;
                 elsif SW_LEVEL = "11" then
-                    if counter = 5 then  -- 50000000
+                    if counter = 50000000 then  -- 50 000 000
                         S <= '1';
                     end if;
                 end if;
